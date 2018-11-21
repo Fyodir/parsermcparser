f6035acaa864fdd90b4b6dcacdb13c29847626d1
Jethro Rainford <rainford1995@gmail.com>
2018-11-21 16:11:00 +0000

    for loop to return exon numbers of input .xml file

--- parserv1.py
+++ parserv1.py
@@ -1,7 +1,20 @@
-#Parsing module
+# importing required modules
 import xml.etree.ElementTree as ET
+import sys
 
-#Inputting XML file
-tree = ET.parse('inputlrg')
+# read input filename from argument
+fileName = sys.argv[1]  # type: xml file
+
+#Check file name is valid .xml
+assert fileName[-4:] == '.xml', 'You have the wrong input file'
+
+# Inputting XML file
+tree = ET.parse(fileName)
 
 root = tree.getroot()
+
+
+# prints exon numbers of lrg file
+for child in root[0][8]:
+    if child.tag == "exon":
+        print("Exon: " + (str(child.attrib))[-4:-2])
