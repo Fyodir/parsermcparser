2bbccfb2da8e680e7638a03158609af16295a6a9
Jethro Rainford <rainford1995@gmail.com>
2018-11-22 11:12:35 +0000

    changed to only select first transcript from XML file

--- parserv1.py
+++ parserv1.py
@@ -4,6 +4,7 @@
 import xml.etree.ElementTree as ET
 import sys
 
+
 # read input filename from argument
 fileName = sys.argv[1]  # type: xml_file
 
@@ -18,7 +19,7 @@ root = tree.getroot()
 
 # prints exon numbers of lrg file
 for child in root[0]:
-    if child.tag == "transcript":
+    if child.tag == "transcript" and child.attrib['name'] == "t1": # only takes the first transcirpt
         for i in child:
             if i.tag == "exon":
                 if (str(i.attrib))[-4] == "'":
@@ -27,4 +28,6 @@ for child in root[0]:
                     print("Exon: " + (str(i.attrib))[-4:-2])
                 elif (str(i.attrib))[-6] == "'":
                     print ("Exon: " + (str(i.attrib))[-5:-2])
+                else:
+                    print("Something is wrong with the XML file")
 
