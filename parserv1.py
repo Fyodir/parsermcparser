943b30a3f8cb4ee968d2d4a0a9e733ed6d2f6f4a
Jethro Rainford <rainford1995@gmail.com>
2018-11-22 12:53:14 +0000

    added function to return two lists with start and end coordinates of exons

--- parserv1.py
+++ parserv1.py
@@ -19,22 +19,43 @@ tree = ET.parse(fileName)
 root = tree.getroot()
 
 
-# Function to return exon numbers from lrg file into a dictionary
-def exon_counter(root):
-    exon_counter_dict = {}
+# function to return exon numbers of lrg file in a list
+def exon_num(root):
+    exon_num_list = []
     for child in root[0]:
         if child.tag == "transcript" and child.attrib['name'] == "t1":  # only takes the first transcirpt
             for i in child:
                 if i.tag == "exon":
                     if (str(i.attrib))[-4] == "'":
-                        exon_counter_dict["Exon: " + (str(i.attrib))[-3:-2]] = None
+                        exon_num_list.append("Exon: " + (str(i.attrib))[-3:-2]) # returns exons 1 - 9
                     elif (str(i.attrib))[-5] == "'":
-                        exon_counter_dict["Exon: " + (str(i.attrib))[-4:-2]] = None
+                        exon_num_list.append("Exon: " + (str(i.attrib))[-4:-2]) # returns exons 10 - 99
                     elif (str(i.attrib))[-6] == "'":
-                        exon_counter_dict["Exon: " + (str(i.attrib))[-5:-2]] = None
+                        exon_num_list.append("Exon: " + (str(i.attrib))[-5:-2]) # returns exons 100 - 999
                     else:
                         print("Something is wrong with the XML file")
-    return(exon_counter_dict)
+    return(exon_num_list)
 
 
-print(exon_counter(root))
+exon_num_var = exon_num(root)
+
+
+# function to return two lists for start and end coordinates of exons respectively
+def exon_coord(root):
+    exon_start_list = []
+    exon_end_list = []
+    for child in root[0]:
+        if child.tag == "transcript" and child.attrib['name'] == "t1":  # only takes the first transcirpt
+            for exon in child:
+                if exon.tag == "exon":
+                   exon_start_list.append(exon[0].attrib["start"])
+            for exon in child:
+                if exon.tag == "exon":
+                   exon_end_list.append(exon[0].attrib["end"])
+    return (exon_start_list, exon_end_list)
+
+
+print(exon_coord(root))
+#print(exon_num_var)
+
+
